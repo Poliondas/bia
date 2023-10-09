e68463c7a701c143df7afc7bb4c256c05d83360f
poliondas <poliondas@gmail.com>
2023-10-09 14:20:47 -0300

    Alteracao no header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -10,7 +10,7 @@ const Header = ({ title, onAdd, showAdd }) => {
       {location.pathname === "/" && (
         <Button
           color={showAdd ? "black" : "green"}
-          text={showAdd ? "Close" : "Add"}
+          text={showAdd ? "Fechar" : "Adicionar"}
           onClick={onAdd}
         />
       )}
